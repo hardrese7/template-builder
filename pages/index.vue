15641095c0da74cb30cced1e5fcdc6ccc3932d21
Rodion Golovushkin <rodion.golovushkin@atnr.pro>
2020-12-10 15:22:08 +0700

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Создайте свой уникальный шаблон из блоков</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
